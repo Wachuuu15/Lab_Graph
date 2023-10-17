24d5d4c41e3db2b16bcae380d266ef39920af74e
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2023-10-16 22:52:58 -0600

    add

--- Raytracer.py
+++ Raytracer.py
@@ -15,7 +15,7 @@ screen.set_alpha(None)
 
 
 raytracer = Raytracer(screen)
-raytracer.envMap = pygame.image.load("pics/sky_pink.png")
+raytracer.envMap = pygame.image.load("pics/rosa_n.jpg")
 raytracer.rtClearColor(0.25,0.25,0.25)
 
 #texture
@@ -23,8 +23,6 @@ starsTexture = pygame.image.load("pics/stars.png")
 mirrorballTexture = pygame.image.load("pics/mirrorball.png")
 skyTexture = pygame.image.load("pics/midnight.png")
 sunsetTexture = pygame.image.load("pics/sunset.png")
-
-
 
 #Materiales
 brick = Material(diffuse=(1,0.4,0.4),spec=8,ks=0.01)
@@ -44,15 +42,34 @@ diamond = Material(diffuse=(0.9,0.9,0.9),spec=128,ks=0.2,ior=2.417,matType=TRANS
 realWater = Material(diffuse=(0.4,0.4,0.9),spec=128,ks=0.2,ior=1.33,matType=TRANSPARENT)
 
 
+#figura 1
+raytracer.scene.append(Triangle(vertices=[(-1,-2,-6),(0,0,-6),(0.1,-2,-5.5)],material=water))
+raytracer.scene.append(Triangle(vertices=[(1.1,-2,-6),(0,0,-6),(0.1,-2,-5.5)],material=mirror))
 
-#raytracer.scene.append(Sphere(position=(0,-1,3),radius=0.15,material=mirror))
+#figura 2
+raytracer.scene.append(Sphere(position=(-2.5,1.5,-7),radius=1,material=mirror))
 
-raytracer.scene.append(Sphere(position=(0,-2,-10),radius= 2, material= brick))
+#figura 3
+raytracer.scene.append(Sphere(position=(-2.5,1.5,-7),radius=1,material=stars))
+
+#figura 4
+raytracer.scene.append(Sphere(position=(-2.5,-1.5,-7),radius=1,material=brick))
+
+#figura 5
+raytracer.scene.append(Sphere(position=(0,-1.5,-7),radius=1,material=sky))
+
+#figura 6
+raytracer.scene.append(Sphere(position=(2.5,1.5,-7),radius=1,material=water))
+
+#figura 7
+raytracer.scene.append(Sphere(position=(1,-2,-3),radius=0.5,material=diamond))
+
+#figura 8
+raytracer.scene.append(Sphere(position=(2,-1,-2),radius=1,material=diamond))
 
 
-
-raytracer.lights.append(AmbientLight(intensity=2))
-raytracer.lights.append(DirectionalLight(direction=(0,0,-1),intensity=0.9))
+raytracer.lights.append(AmbientLight(intensity=0.1))
+raytracer.lights.append(DirectionalLight(direction=(-1,-1,-1),intensity=0.9))
 
 #raytracer.lights.append(PointLight(point=(1.5,0,-5),intensity=1,color=(1,0,1)))
 
