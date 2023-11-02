d08b9299da9e5d64ec8995c2c57ffb7a524a246c
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2023-11-02 12:55:37 -0600

    Update

--- materials.py
+++
@@ -1,7 +0,0 @@
-class Material(object):
-
-#determina como se comporta la luz cuando le pega a una superficie    
-    def __init__(self, diffuse = (1,1,1), spec= 1.0, ks = 0.0):
-        self.diffuse = diffuse
-        self.spec = spec
-        self.ks = ks
