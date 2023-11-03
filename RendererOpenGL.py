67b04842f9b16e8d1706ff30321f08170b421c0b
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2023-11-02 22:02:52 -0600

    End obj

--- RendererOpenGL.py
+++ RendererOpenGL.py
@@ -6,6 +6,7 @@ import glm
 from gl import Renderer
 from model import Model
 from shaders import *
+from obj import Obj
 
 width = 960
 height = 540
@@ -19,10 +20,59 @@ rend = Renderer(screen)
 rend.setShaders(vertex_shader,fragment_shader)
 
 
-modelo = Model(filename="obj/12221_Cat_v1_l3.obj",translate=glm.vec3(0,-1,-5),rotation=glm.vec3(-90,0,0),scale=glm.vec3(1,1,1))
+obj = Obj(filename="obj/12221_Cat_v1_l3.obj")
+objData = []
+
+for face in obj.faces:
+
+    if len(face) == 3:
+
+        for vertexInfo in face:
+
+            vertexID, texcoordID, normalID = vertexInfo
+            vertex = obj.vertices[vertexID - 1]
+            normals = obj.normals[normalID - 1]
+            uv = obj.texcoords[texcoordID - 1]
+            uv = [uv[0], uv[1]]
+
+            objData.extend(vertex + uv + normals)
+
+    elif len(face) == 4:
+
+        for i in [0, 1, 2]:
+
+            vertexInfo = face[i]
+            vertexID, texcoordID, normalID = vertexInfo
+            vertex = obj.vertices[vertexID - 1]
+            normals = obj.normals[normalID - 1]
+            uv = obj.texcoords[texcoordID - 1]
+
+            uv = [uv[0], uv[1]]
+
+            objData.extend(vertex + uv + normals)
+
+        for i in [0, 2, 3]:
+
+            vertexInfo = face[i]
+
+            vertexID, texcoordID, normalID = vertexInfo
+            vertex = obj.vertices[vertexID - 1]
+            normals = obj.normals[normalID - 1]
+            uv = obj.texcoords[texcoordID - 1]
+            uv = [uv[0], uv[1]]
+
+            objData.extend(vertex + uv + normals)
+
+
+model = Model(objData)
+
 modelo.loadTexture("texture/Cat_diffuse.jpg")
 
-rend.scene.append(modelo)
+model.position.z = -6
+model.position.y = 0
+model.scale = glm.vec3(0.01, 0.01, 0.01)
+renderer.scene.append(model)
+
 
 
 #                 # POSITIONS,    COLORS
